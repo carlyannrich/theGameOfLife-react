ac73de6f7d9c19da495c8827f4083b1d50334f9d
Carly Richardson <63616608+carlyannrich@users.noreply.github.com>
2021-01-13 00:01:34 +0000

    set up react app and began creating Game component

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Game from "./Game.js";
+
+function App() {
+  return <Game />;
+}
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"));
